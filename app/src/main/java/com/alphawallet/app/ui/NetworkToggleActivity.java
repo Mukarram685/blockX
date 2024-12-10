abe2964f526dcc4937900f16934ce966477aa625
Mukarram685 <muka654r@gmail.com>
2024-12-10 15:20:01 +0500

    metanova project

--- app/src/main/java/com/alphawallet/app/ui/NetworkToggleActivity.java
+++ app/src/main/java/com/alphawallet/app/ui/NetworkToggleActivity.java
@@ -52,8 +52,17 @@ public class NetworkToggleActivity extends NetworkBaseActivity
 
     private void setupFilterList()
     {
+    // Get the existing network lists
         List<NetworkItem> mainNetList = viewModel.getNetworkList(true);
         List<NetworkItem> testNetList = viewModel.getNetworkList(false);
+
+    // Add BlockX and MetaNova at the top of the mainNetList
+    NetworkItem blockXNetwork = new NetworkItem(String.valueOf("BlockX MainNet"),19191L, true); // Always selected
+    NetworkItem metaNovaNetwork = new NetworkItem(String.valueOf("MetaNova Verse"),10096L, true); // Always selected
+
+    // Ensure these are added at the top of the list
+        mainNetList.add(0, blockXNetwork);
+        mainNetList.add(1, metaNovaNetwork);
 
         MultiSelectNetworkAdapter.Callback callback = new MultiSelectNetworkAdapter.Callback()
         {
@@ -106,10 +115,16 @@ public class NetworkToggleActivity extends NetworkBaseActivity
             @Override
             public void onCheckChanged(long chainId, int count)
             {
+            // Prevent unselecting BlockX and MetaNova
+            if (String.valueOf(chainId).equals("19191") || String.valueOf(chainId).equals("10096"))
+            {
+                return; // Ignore any changes to these items
+            }
                 updateTitle();
             }
         };
 
+    // Initialize adapters with the updated lists
         mainNetAdapter = new MultiSelectNetworkAdapter(mainNetList, callback);
         mainnetRecyclerView.setAdapter(mainNetAdapter);
 
@@ -118,6 +133,8 @@ public class NetworkToggleActivity extends NetworkBaseActivity
 
         updateTitle();
     }
+
+
 
     @Override
     protected void updateTitle()
@@ -152,4 +169,5 @@ public class NetworkToggleActivity extends NetworkBaseActivity
         setResult(RESULT_OK, new Intent());
         finish();
     }
+
 }
